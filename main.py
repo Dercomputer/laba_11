e093099a4eaca08c6dd3b7ea1f574f03fd6dc254
Der_computer <tafince16.05@gmail.com>
2025-12-07 20:19:37 +0300

    added frontend-beta

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
 import sqlite3
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class User(BaseModel):
+    name: str = "Guest"
+    password: str = None
+
+
 
 @app.get("/")
 async def root():
     return {1: 2}
+
+
+@app.get("/show")
+async def show_message():
+    ...
+
+
+@app.delete("/delete")
+async def delete_message():
+    ...
+
+
+@app.patch("/change_password")
+async def change_password():
+    ...
+
+
+@app.post("/register")
+async def register():
+    ...
+
+
+@app.get("/login")
+async def login():
+    ...
+
+
+@app.post("/send")
+async def send_message():
+    ...
+
 
 if __name__ == "__main__":
     import uvicorn
